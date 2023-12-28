47c3191bc0ff03df5212258b33b9a2085c00f949
Aomd <g00665@163.com>
2023-12-28 17:04:09 +0800

    添加 输出测试报告功能

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-
-/** 
+/**
  * @link https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
- * @type {import('ts-jest').JestConfigWithTsJest} 
+ * @type {import('ts-jest').JestConfigWithTsJest}
  */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    "default",
+    ["jest-junit", { outputName: '/dist/junit.xml' }]
+  ]
 };
